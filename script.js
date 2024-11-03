2229183022af98026abe4c370e50286be219af9a
ErikLeeQuesada <erikleeque@gmail.com>
2024-11-02 22:56:34 -0600

    Initial File Upload Before Google Tags

---
+++ script.js
@@ -0,0 +1,33 @@
+const images = [
+    'Office1.jpg', // First image
+    'Office2.jpg', // Second image
+    'Office3.jpg',
+    'Office4.jpg',
+    'Office5.jpg',
+    'Office6.jpg', // Third image
+];
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+let currentImageIndex = 0;
+
+document.addEventListener('DOMContentLoaded', function () {
+    const hero = document.querySelector('.hero');
+
+    setInterval(() => {
+        setTimeout(() => {
+            // Change background image after fade-out completes
+            currentImageIndex = (currentImageIndex + 1) % images.length;
+            hero.style.backgroundImage = `url('${images[currentImageIndex]}')`;
+            hero.classList.remove('hidden'); // Trigger fade-in
+        }, 1000); // Delay for fade-out duration
+    }, 7000); // Image Transition every 7 seconds 
+});
+
